4f2f0b76cfdf7ee89bfb69340a3510ca20ca4acd
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 18:41:26 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -11,14 +11,20 @@ class MyPackageServiceProvider extends ServiceProvider
         // ...
     }
 
-
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/CustomAdminPackage/layout.blade.php'),
-            __DIR__ . '/Http/Controllers' => app_path('Http/Controllers'),
-            __DIR__ . '/Routes/web.php' => base_path('routes/web.php'),
+            __DIR__ . '/../resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/vendor/myCustomAdminPackage/layout.blade.php'),
+            __DIR__ . '/../Http/Controllers' => app_path('Http/Controllers'),
         ], 'my-package-resources');
+
+        $this->addRoutes();
     }
-    
+
+    protected function addRoutes()
+    {
+        $routeFile = base_path('routes/web.php');
+        $routes = "\nRoute::get('/your-route', 'YourController@yourMethod');";
+        file_put_contents($routeFile, $routes, FILE_APPEND | LOCK_EX);
+    }
 }
