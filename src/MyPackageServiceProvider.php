41225d7221be7247c519c4a4cf835c5a24f264ac
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 17:58:07 +0500

    check

---
+++ src/MyPackageServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Panelist\AdminPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyPackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // ...
+    }
+
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/MyCustomPackage.php' => config_path('MyCustomPackage.php'),
+            __DIR__ . '/../../resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/myCustomAdminPackage/layout.blade.php'),
+        ]);
+    }
+}
