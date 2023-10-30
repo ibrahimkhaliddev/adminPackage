ea48b281b7753f9365614443099271daaf36184d
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 18:55:54 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -15,8 +15,12 @@ class MyPackageServiceProvider extends ServiceProvider
     {
         $myWebPhpContents = file_get_contents(__DIR__ . '/Routes/web.php');
 
+        // Remove the line "use Illuminate\Support\Facades\Route;" from the package's web.php content
+        $myWebPhpContents = str_replace("use Illuminate\Support\Facades\Route;\n", '', $myWebPhpContents);
+
         $existingWebPhpContents = file_get_contents(base_path('routes/web.php'));
 
+        // Append the modified package's web.php content to the main project's web.php
         file_put_contents(base_path('routes/web.php'), $myWebPhpContents, FILE_APPEND | LOCK_EX);
 
         $this->publishes([
