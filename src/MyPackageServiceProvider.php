4797079e87ab4d481042b916f8504909525e22eb
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 18:59:41 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -15,13 +15,12 @@ class MyPackageServiceProvider extends ServiceProvider
     {
         $myWebPhpContents = file_get_contents(__DIR__ . '/Routes/web.php');
 
-        // Remove the line "use Illuminate\Support\Facades\Route;" from the package's web.php content
-        $myWebPhpContents = str_replace("use Illuminate\Support\Facades\Route;\n", '', $myWebPhpContents);
+        preg_match('/use App\\Http\\Controllers\\MenuController;/', $myWebPhpContents, $matches);
+        $extractedLine = isset($matches[0]) ? $matches[0] : '';
 
         $existingWebPhpContents = file_get_contents(base_path('routes/web.php'));
 
-        // Append the modified package's web.php content to the main project's web.php
-        file_put_contents(base_path('routes/web.php'), $myWebPhpContents, FILE_APPEND | LOCK_EX);
+        file_put_contents(base_path('routes/web.php'), $extractedLine, FILE_APPEND | LOCK_EX);
 
         $this->publishes([
             __DIR__ . '/resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/CustomAdminPackage/layout.blade.php'),
