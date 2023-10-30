167ccb2ad4b3a618d040dcf3470f978070b24983
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 20:47:33 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -28,24 +28,26 @@ class MyPackageServiceProvider extends ServiceProvider
     {
         $projectWebPath = base_path('routes/web.php');
         $packageWebContents = file_get_contents(__DIR__ . '/Routes/web.php');
-
-        $additionalLines = "\nuse App\Http\Controllers\SlackController;\n";
-
+    
+        $additionalLines = "use App\Http\Controllers\HomeController;\nuse App\Http\Controllers\MenuController;\nuse App\Http\Controllers\SlackController;\n";
+        $additionalLines = str_replace("\\", "\\\\", $additionalLines); // Escape backslashes
+    
         $projectWebContents = file_get_contents($projectWebPath);
-        $pattern = '/<\?php/';
-        $projectWebContents = preg_replace($pattern, "<?php" . $additionalLines, $projectWebContents, 1);
-
+        $pattern = '/<\?php\s*/';
+        $projectWebContents = preg_replace($pattern, "<?php\n\n" . $additionalLines, $projectWebContents, 1);
+    
         $packageWebContents = str_replace('<?php', '', $packageWebContents); // Remove opening PHP tag
-
+    
         if (strpos($projectWebContents, $packageWebContents) === false) {
             // Add new route group with proper formatting
-            $packageWebContents = str_replace('Route::middleware', "\n\nRoute::middleware", $packageWebContents);
-
+            $packageWebContents = "\nRoute::middleware(['auth'])->group(function () {\n" . $packageWebContents . "\n});\n";
+    
             // Append the package's routes
             $projectWebContents .= $packageWebContents;
-
+    
             // Write the updated content back to the file
             file_put_contents($projectWebPath, $projectWebContents);
         }
     }
+    
 }
