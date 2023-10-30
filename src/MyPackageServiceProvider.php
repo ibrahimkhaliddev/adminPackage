776988fcae9f37fdc3e99d7f8de307b8c89b4eb1
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 18:52:32 +0500

    done

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -13,18 +13,16 @@ class MyPackageServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Get the contents of the my web.php file.
         $myWebPhpContents = file_get_contents(__DIR__ . '/Routes/web.php');
 
-        // Write the contents of the my web.php file to the existing web.php file.
-        file_put_contents(base_path('routes/web.php'), $myWebPhpContents);
+        $existingWebPhpContents = file_get_contents(base_path('routes/web.php'));
 
-        // Publish the myCustomAdminPackage/layout.blade.php file to the resources/views/CustomAdminPackage/layout.blade.php file.
+        file_put_contents(base_path('routes/web.php'), $myWebPhpContents, FILE_APPEND | LOCK_EX);
+
         $this->publishes([
             __DIR__ . '/resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/CustomAdminPackage/layout.blade.php'),
         ], 'my-package-resources');
 
-        // Publish the Http/Controllers directory to the app_path('Http/Controllers') directory.
         $this->publishes([
             __DIR__ . '/Http/Controllers' => app_path('Http/Controllers'),
         ], 'my-package-resources');
