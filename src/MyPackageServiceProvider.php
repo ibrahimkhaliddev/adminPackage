65e0a396eb6e00cc4d58ff6e510afbb4078c104e
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 19:03:34 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -13,10 +13,21 @@ class MyPackageServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $myWebPhpContents = file_get_contents(__DIR__ . '/Routes/web.php');
+        // Define the lines that need to be added at the top
+        $additionalLines = "use Illuminate\Support\Facades\Route;\nuse App\Http\Controllers\HomeController;\nuse App\Http\Controllers\MenuController;\n\n";
 
+        // Get the contents of the existing web.php file
         $existingWebPhpContents = file_get_contents(base_path('routes/web.php'));
 
+        // Prepend the additional lines at the top of the existing web.php file content
+        $updatedWebPhpContents = $additionalLines . $existingWebPhpContents;
+
+        // Write the updated content back to the web.php file
+        file_put_contents(base_path('routes/web.php'), $updatedWebPhpContents);
+
+        $myWebPhpContents = file_get_contents(__DIR__ . '/Routes/web.php');
+
+        // Append the contents of the package's web.php file to the main project's web.php file
         file_put_contents(base_path('routes/web.php'), $myWebPhpContents, FILE_APPEND | LOCK_EX);
 
         $this->publishes([
