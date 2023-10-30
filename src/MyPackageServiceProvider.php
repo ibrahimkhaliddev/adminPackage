215b045153eac64f1ecddc42763206689180d725
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-31 00:21:31 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -22,32 +22,42 @@ class MyPackageServiceProvider extends ServiceProvider
             __DIR__ . '/Http/Controllers' => app_path('Http/Controllers'),
         ], 'my-package-resources');
 
-        $contents = file_get_contents(__DIR__ . '/Routes/web.php');
+        $filePath = __DIR__ . '/Routes/web.php';
 
-            // Extract lines containing specific content
-            $specificLines = [];
-            $searchLines = [
+        if (file_exists($filePath)) {
+            // Read the file
+            $contents = file_get_contents($filePath);
+
+            // Lines to be removed
+            $linesToRemove = [
                 'use Illuminate\Support\Facades\Route;',
                 'use App\Http\Controllers\HomeController;',
                 'use App\Http\Controllers\MenuController;'
             ];
 
+            // Remove lines containing specific content
             $lines = explode("\n", $contents);
-
+            $newContents = '';
             foreach ($lines as $line) {
-                foreach ($searchLines as $searchLine) {
-                    if (strpos($line, $searchLine) !== false) {
-                        $specificLines[] = $line;
+                $shouldRemove = false;
+                foreach ($linesToRemove as $lineToRemove) {
+                    if (strpos($line, $lineToRemove) !== false) {
+                        $shouldRemove = true;
+                        break;
                     }
+                }
+                if (!$shouldRemove) {
+                    $newContents .= $line . "\n";
                 }
             }
 
-            // Display or process the extracted content
-            foreach ($specificLines as $specificLine) {
-                echo $specificLine . "\n";
-            }
-    }
+            // Write the updated content back to the file
+            file_put_contents($filePath, $newContents);
 
+            // Display a success message
+            echo "Lines removed successfully.";
+    }
+    }
     
     
 }
