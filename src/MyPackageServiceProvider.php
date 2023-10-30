611ef9905de2db44191ce209905bfa49b46460ec
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-30 19:20:12 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -13,28 +13,7 @@ class MyPackageServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Define the lines that need to be added at the top
-        $additionalLines = "use App\Http\Controllers\HomeController;\nuse App\Http\Controllers\MenuController;\n\n";
-
-        // Get the contents of the existing web.php file
-        $existingWebPhpContents = file_get_contents(base_path('routes/web.php'));
-
-        // Check if the lines already exist at the top; if they don't, prepend the additional lines
-        if (strpos($existingWebPhpContents, $additionalLines) !== 0) {
-            $existingWebPhpContents = $additionalLines . $existingWebPhpContents;
-        }
-
-        // Write the updated content back to the web.php file
-        file_put_contents(base_path('routes/web.php'), $existingWebPhpContents);
-
-        // Remove the duplicate 'use Illuminate\Support\Facades\Route;'
-        $myWebPhpContents = str_replace("use Illuminate\Support\Facades\Route;\n", '', file_get_contents(__DIR__ . '/Routes/web.php'));
-
-        // Check if the package's web.php content already exists in the project's web.php file
-        if (strpos($existingWebPhpContents, $myWebPhpContents) === false) {
-            // Append the contents of the package's web.php file to the main project's web.php file
-            file_put_contents(base_path('routes/web.php'), $myWebPhpContents, FILE_APPEND | LOCK_EX);
-        }
+        $this->appendWebRoutes();
 
         $this->publishes([
             __DIR__ . '/resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/CustomAdminPackage/layout.blade.php'),
@@ -43,5 +22,25 @@ class MyPackageServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/Http/Controllers' => app_path('Http/Controllers'),
         ], 'my-package-resources');
+    }
+
+    private function appendWebRoutes()
+    {
+        $projectWebPath = base_path('routes/web.php');
+        $packageWebPath = __DIR__ . '/Routes/web.php';
+
+        $projectWebContents = file_get_contents($projectWebPath);
+        $packageWebContents = str_replace("use Illuminate\Support\Facades\Route;\n", '', file_get_contents($packageWebPath));
+
+        $additionalLines = "use App\Http\Controllers\HomeController;\nuse App\Http\Controllers\MenuController;\n\n";
+
+        if (strpos($projectWebContents, $additionalLines) !== 0) {
+            $projectWebContents = $additionalLines . $projectWebContents;
+        }
+
+        if (strpos($projectWebContents, $packageWebContents) === false) {
+            $projectWebContents .= $packageWebContents;
+            file_put_contents($projectWebPath, $projectWebContents);
+        }
     }
 }
