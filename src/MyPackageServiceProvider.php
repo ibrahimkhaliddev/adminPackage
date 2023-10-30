ff160c4cf77dabf89a1c586687a57b6b4fef04c1
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-31 00:06:09 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -13,7 +13,6 @@ class MyPackageServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->appendWebRoutes();
 
         $this->publishes([
             __DIR__ . '/resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/CustomAdminPackage/layout.blade.php'),
@@ -22,47 +21,10 @@ class MyPackageServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/Http/Controllers' => app_path('Http/Controllers'),
         ], 'my-package-resources');
+
+        $this->loadRoutesFrom(__DIR__.'/Routes/web.php');
     }
 
-    private function appendWebRoutes()
-    {
-        $projectWebPath = base_path('routes/web.php');
-        $packageWebContents = file_get_contents(__DIR__ . '/Routes/web.php');
-    
-        $additionalLines = "use App\Http\Controllers\HomeController;\nuse App\Http\Controllers\MenuController;\nuse App\Http\Controllers\SlackController;\n";
-        $additionalLines = str_replace("\\", "\\\\", $additionalLines); // Escape backslashes
-    
-        $projectWebContents = file_get_contents($projectWebPath);
-        $pattern = '/<\?php\s*/';
-        $projectWebContents = preg_replace($pattern, "<?php\n\n" . $additionalLines, $projectWebContents, 1);
-    
-        // Check for duplicate entries
-        $useStatements = [
-            'use App\Http\Controllers\HomeController;',
-            'use App\Http\Controllers\MenuController;',
-            'use App\Http\Controllers\SlackController;',
-            'use Illuminate\Support\Facades\Route;'
-        ];
-    
-        foreach ($useStatements as $useStatement) {
-            if (strpos($projectWebContents, $useStatement) === false) {
-                $projectWebContents = str_replace('<?php', $useStatement . "\n\n<?php", $projectWebContents);
-            }
-        }
-    
-        $packageWebContents = str_replace('<?php', '', $packageWebContents); // Remove opening PHP tag
-    
-        if (strpos($projectWebContents, $packageWebContents) === false) {
-            // Add new route group with proper formatting
-            $packageWebContents = "\nRoute::middleware(['auth'])->group(function () {\n" . $packageWebContents . "\n});\n";
-    
-            // Append the package's routes
-            $projectWebContents .= $packageWebContents;
-    
-            // Write the updated content back to the file
-            file_put_contents($projectWebPath, $projectWebContents);
-        }
-    }
     
     
 }
