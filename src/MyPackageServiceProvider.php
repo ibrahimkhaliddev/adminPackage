e4889acc2ab10e324af5351aff489ac6e1c22b93
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-11-03 19:18:09 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -175,7 +175,7 @@ class MyPackageServiceProvider extends ServiceProvider
         $mergedLines = array_unique(array_merge($filteredLines, $linesToInsert, $originalFilteredLines, explode("\n", $packageWebContent)));
         $resultContent = implode("\n", $mergedLines);
 // print_r(filteredLines);
-file_put_contents($sampleWebPath, implode("\n", $originalWebContent));
+file_put_contents($sampleWebPath, implode("\n", $originalFilteredLines));
 die();
         if (trim(end($mergedLines)) !== '});') {
             $resultContent .= "\n});";
