39f80ec30d4ce7819123714f04b3338073997548
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-11-04 19:21:30 +0500

    done

--- src/Database/seeders/stUserMenuSeeder.php
+++ src/Database/seeders/stUserMenuSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\stUserMenu;
 
-class stUserMenus extends Seeder
+class stUserMenuSeeder extends Seeder
 {
     public function run()
     {
